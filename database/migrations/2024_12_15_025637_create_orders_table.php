68ceafb1ca046091f1d932c68e7477877b1a50e6
Sebastian <sebastiantriana1911@gmail.com>
2024-12-14 22:58:37 -0500

    lifting of migrations

---
+++ database/migrations/2024_12_15_025637_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_client');
+            $table->string('phone_client');
+            $table->string('address');
+            $table->timestamp('date_order')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('state')->default('Pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
